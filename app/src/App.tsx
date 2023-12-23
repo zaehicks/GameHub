eb1a91b796a1f4822d6fd9ca5f64557a73fc0b53
zaehicks <isaiahhickerson@gmail.com>
2023-12-22 18:34:27 -0800

    added filtering platforms

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -8,6 +8,7 @@ import PlatformSelector from "./components/PlatformSelector";
 
 function App() {
   const [selectedGenre, setSelectedGenre] =useState<Genre | null>(null); 
+  const [selectedPlatform, setSelectedPlatform] = useState<Platform | null>(null);
 
   return (
     <Grid templateAreas={{
@@ -28,8 +29,8 @@ function App() {
         </GridItem>
       </Show>
       <GridItem area="main" >
-        <PlatformSelector />
-        <GameGrid selectedGenre={selectedGenre}/>
+        <PlatformSelector selectedPlatform={selectedPlatform} onSelectPlatform={(platform) => setSelectedPlatform(platform) }/>
+        <GameGrid selectedPlatform={selectedPlatform} selectedGenre={selectedGenre}/>
       </GridItem>
     </Grid>
   );
