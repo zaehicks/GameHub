4ca4b8f6aad6094e511d3e3552b4d35bb56c266f
zaehicks <isaiahhickerson@gmail.com>
2023-12-23 13:04:32 -0800

    refactored some code, added new logo

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -5,32 +5,45 @@ import GenreList from "./components/GenreList";
 import { useState } from "react";
 import { Genre } from "./hooks/useGenres";
 import PlatformSelector from "./components/PlatformSelector";
+import { Platform } from "./hooks/useGames";
+
+export interface GameQuery {
+  genre: Genre | null;
+  platform: Platform | null;
+}
 
 function App() {
-  const [selectedGenre, setSelectedGenre] =useState<Genre | null>(null); 
-  const [selectedPlatform, setSelectedPlatform] = useState<Platform | null>(null);
-
+  const [gameQuery, setGameQuery] = useState<GameQuery>({} as GameQuery);
   return (
-    <Grid templateAreas={{
-      base: `"nav" "main"`,
-      lg: `"nav nav" "aside main"` // 1024px
-    }}
-    templateColumns={{
-      base: '1fr',
-      lg: '200px 1fr'
-    }}
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`, // 1024px
+      }}
+      templateColumns={{
+        base: "1fr",
+        lg: "200px 1fr",
+      }}
     >
       <GridItem area="nav">
         <NavBar />
       </GridItem>
       <Show above="lg">
         <GridItem area="aside" paddingX={5}>
-          <GenreList selectedGenre={selectedGenre} onSelectGenre={(genre) => setSelectedGenre(genre)}/>
+          <GenreList
+            selectedGenre={gameQuery.genre}
+            onSelectGenre={(genre) => setGameQuery({ ...gameQuery, genre })}
+          />
         </GridItem>
       </Show>
-      <GridItem area="main" >
-        <PlatformSelector selectedPlatform={selectedPlatform} onSelectPlatform={(platform) => setSelectedPlatform(platform) }/>
-        <GameGrid selectedPlatform={selectedPlatform} selectedGenre={selectedGenre}/>
+      <GridItem area="main">
+        <PlatformSelector
+          selectedPlatform={gameQuery.platform}
+          onSelectPlatform={(platform) =>
+            setGameQuery({ ...gameQuery, platform })
+          }
+        />
+        <GameGrid gameQuery={gameQuery} />
       </GridItem>
     </Grid>
   );
--- app/src/components/NavBar.tsx
+++ app/src/components/NavBar.tsx
@@ -1,11 +1,11 @@
 import { HStack, Image } from '@chakra-ui/react'
-import logo from '../assets/logo.webp';
+import logo from '../assets/skydecklogo.svg';
 import ColorModeSwitch from './ColorModeSwitch';
 
 const NavBar = () => {
   return (
     <HStack justifyContent='space-between' padding='10px'>
-      <Image src={logo} boxSize='60px'/>
+      <Image src={logo} boxSize='120px' marginLeft={8}/>
       <ColorModeSwitch />
     </HStack>
   )
