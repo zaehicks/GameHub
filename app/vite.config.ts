acf3d156c2853cb88719af0c62c1bc86f409ab3b
zaehicks <isaiahhickerson@gmail.com>
2024-01-04 23:34:11 -0500

    deploy test

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  // GitHub Pages expects an index.html in the root directory
+  // so just run npm build before pushing to GitHub and this will rebuild our assets to the root
+  build: { outDir: '..' },
+  // needed for github pages just put the repo name here
+  base: '/GameHub/', 
+});
