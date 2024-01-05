bb864d5d8c91aec6fa647bde39d34765713d7836
zaehicks <isaiahhickerson@gmail.com>
2024-01-04 23:44:20 -0500

    t

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -3,7 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: { outDir: '..' },
   plugins: [react()],
   base: '/GameHub/',
 })
+
+
+
 
