3c29a04f95cd7c765ddec79542477b097b21aa35
zaehicks <isaiahhickerson@gmail.com>
2024-01-06 14:47:58 -0500

    built search input component

--- app/src/components/NavBar.tsx
+++ app/src/components/NavBar.tsx
@@ -1,11 +1,13 @@
 import { HStack, Image } from '@chakra-ui/react'
 import logo from '../assets/skydecklogo.svg';
 import ColorModeSwitch from './ColorModeSwitch';
+import SearchInput from './SearchInput';
 
 const NavBar = () => {
   return (
-    <HStack justifyContent='space-between' padding='10px'>
+    <HStack padding='10px'>
       <Image src={logo} boxSize='120px' marginLeft={8}/>
+      <SearchInput/>
       <ColorModeSwitch />
     </HStack>
   )
