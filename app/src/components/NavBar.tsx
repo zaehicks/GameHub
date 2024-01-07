cab6f8cd3a10799acd933c72b5fc6db195de85cf
zaehicks <isaiahhickerson@gmail.com>
2024-01-07 13:48:41 -0500

    made searchbar fully functional

--- app/src/components/NavBar.tsx
+++ app/src/components/NavBar.tsx
@@ -3,11 +3,15 @@ import logo from '../assets/skydecklogo.svg';
 import ColorModeSwitch from './ColorModeSwitch';
 import SearchInput from './SearchInput';
 
-const NavBar = () => {
+interface Props {
+  onSearch: (searchText: string) => void;
+}
+
+const NavBar = ({ onSearch }: Props) => {
   return (
     <HStack padding='10px'>
       <Image src={logo} boxSize='120px' marginLeft={8}/>
-      <SearchInput/>
+      <SearchInput onSearch={onSearch}/>
       <ColorModeSwitch />
     </HStack>
   )
